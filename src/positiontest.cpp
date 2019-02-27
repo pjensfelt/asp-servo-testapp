06c34af379295fd0a844fed3479bb3d4114810a5
Patric Jensfelt <patric@kth.se>
2019-02-27 11:46:09 +0100

    Sending a single position command

--- src/positiontest.cpp
+++ src/positiontest.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
-#include <cmath>
 
 int main() {
 
@@ -29,20 +28,9 @@ int main() {
         std::string delimiter = ",";
         std::string servo_name = input_str.substr(0, input_str.find(delimiter));
         int target_position = std::stoi(input_str.substr(input_str.find(delimiter)+1,input_str.size() - input_str.find(delimiter) - 1));
-        if (target_position > 1310720) {
-            target_position = 1310720;
-        }
-        if (target_position < -1310720) {
-            target_position = -1310720;
-        }
+
         servo_collection.set_verbose(true);
-        for (int i = 0; i < 101; i++) {
-            double k = std::abs(((double) i-50) / 50.0);
-            int posi = (1-k) * target_position;
-            // Writing position to servo
-            servo_collection.write(servo_name,"Position",posi);
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        }
+        servo_collection.write(servo_name,"Position",target_position);
         servo_collection.set_verbose(false);
     }
 
