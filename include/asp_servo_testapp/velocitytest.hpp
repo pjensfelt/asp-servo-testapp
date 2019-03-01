8a7bb6fdbb2631f2f059eaca1fa68bd0a724b31e
Carolina <carolinabianchi95@gmail.com>
2019-03-01 16:05:42 +0100

    Add first watchdog version

---
+++ include/asp_servo_testapp/velocitytest.hpp
@@ -0,0 +1,129 @@
+#include <asp_servo_api/asp_servo.h>
+#include <asp_servo_api/servo.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <map>
+#include <csignal>
+#include <string.h>
+#include <semaphore.h>
+
+#define MAX_V 		1310720
+
+// Constants
+const std::string CONFIG 	= "../config/" ;
+const std::string VEL_CONF 	= "asp_test_velocity.xml" ;
+const int INPUT_LEN 		= 255 ;
+
+// Maps non-standard name of a servo, to the standard convention
+std::map<std::string, std::string> servoname = 
+	{
+		{"s1", "s1"},
+		{"s2", "s2"},
+		{"s3", "s3"},
+		{"s4", "s4"},
+		{"s5", "s5"},
+
+		{"S1", "s1"},
+		{"S2", "s2"},
+		{"S3", "s3"},
+		{"S4", "s4"},
+		{"S5", "s5"},
+
+		{"z", "s1"},
+		{"x", "s2"},
+		{"y", "s3"},
+		{"b", "s4"},
+		{"a", "s5"},
+
+		{"Z", "s1"},
+		{"X", "s2"},
+		{"Y", "s3"},
+		{"B", "s4"},
+		{"A", "s5"}	
+	};
+
+std::map<std::string, double> scale = 
+	{
+
+		{"s1",  5044.0}, // Z -> ticks/mm 
+		{"s2",  3991.8}, // X -> ticks/mm
+		{"s3",  5099.4}, // Y -> ticks/mm
+		{"s4", 32768.0}, // B -> ticks/°
+		{"s5", 27307.0}, // A -> ticks/°
+	};
+
+/**
+* Parses a command and fills the value of servo and velocity.
+* The accepted format is:
+* servoname velocity
+* Velocity is expressed in mm/s or °/s
+* Servoname can either be s1/s2/../S1/S2../x/y/.../X/Y/... as in the servoname map.
+*/
+void parse_command (std::string command, std::string &servo, float &vel)
+{
+	std::string delimiter = " ";
+	size_t pos;
+	std::map<std::string, std::string>::iterator it;
+	
+	pos = command.find(delimiter);
+	
+	// There is no " " in the string, or it is at the beginning of the command
+	if(pos == std::string::npos || pos == 0)
+	{
+		std::cout << "Command:" << command << std::endl;
+		throw std::runtime_error("Cannot parse the command \n");
+	}
+	
+	std::string name = command.substr(0, pos); 
+	it = servoname.find(name);
+	
+	// Suitable conversion for the servo name not found
+	if(it == servoname.end())
+	{
+		throw std::runtime_error("Unknown servo name \n");
+	}
+	servo = it->second;
+	
+	try
+	{
+		vel = std::stof(command.substr(pos+1, command.size() - pos - 1)); 
+	} catch (std::invalid_argument e){
+		throw std::runtime_error("Cannot parse command. Check velocity format.\n");
+	}
+
+}
+
+/**
+* Converts a velocity into the number of ticks/s to be inputted to the servo motor.
+* The velocity has to be expressed in mm/s in case of s1, s2, s3 or °/s in case of A and B.
+*/
+int to_ticks(float velocity, std::string servo)
+{
+	std::map<std::string, double>::iterator it = scale.find(servo);
+	if(it == scale.end())
+	{
+		throw std::runtime_error("This error is due to a programming error, check it out!\n");
+	}
+	std::cout << "Scale factor " << it->second << std::endl;
+	return (int) it->second*velocity;
+}
+
+void stop_all(asp::ServoCollection *servo_collection);
+
+//void stop_all(int sig);
+
+/**
+* Attemp to provide a clean exit by executing stop_all when a signal is catched.
+*/
+/*
+void register_handlers(){
+
+	std::signal(SIGINT, stop_all);
+	std::signal(SIGABRT, stop_all);
+	std::signal(SIGFPE, stop_all);
+	std::signal(SIGILL, stop_all);
+	std::signal(SIGSEGV, stop_all);
+	std::signal(SIGTERM, stop_all);
+
+}*/
--- src/velocitytest.cpp
+++ src/velocitytest.cpp
@@ -1,27 +1,68 @@
-#include <asp_servo_api/asp_servo.h>
-#include <asp_servo_api/servo.h>
-#include <iostream>
-#include <chrono>
-#include <thread>
+#include "../include/asp_servo_testapp/velocitytest.hpp"
 
-int main(int argc, char** argv) {
+#define CTRL_LOOP 1000 
+#define WRITE_MAX 100
+#define WATCHDOG_LOOP 3000 // The watchdog has a loop that is twice as long as the main thread
 
-    bool verbose = true;
-    if (argc > 1) {
-	verbose = false;
-    }
+int write_cnt = 0;
+sem_t semaphore;
 
-    // Loading servo settings from XML
-    asp::ServoCollection servo_collection("../config/asp_test_velocity.xml");
+void *watchdog_fct(void *args){
 
+	std::cout << "Watchdog function up" << std::endl;
+	
+	// Variables
+	asp::ServoCollection *servo_collection = (asp::ServoCollection *)args;
+	int prev_count = -2;
+	int read_val = -1;
+	
+	// Wait the control thread to be operative
+	sem_wait(&semaphore);
+	
+	do{		
+		prev_count = read_val;
+		read_val = write_cnt;
+		std::this_thread::sleep_for(std::chrono::milliseconds(WATCHDOG_LOOP));
+	}while(read_val != prev_count);
+	
+	// If we're here, the ctrl_fucntion hasn't updated the write count in a while -> emergency
+	std::cout << "Watchdog TIMEOUT after " << WATCHDOG_LOOP << " ms,  stopping all" << std::endl;
+	stop_all(servo_collection);
+	
+	std::cout << "Watchdog exiting "<< std::endl;
+	pthread_exit(0);	
+}
+
+void stop_all(asp::ServoCollection *servo_collection){
+	std::map<std::string, double>::iterator it;
+	
+	// Write 0 to all the servos
+	for( it = scale.begin(); it!= scale.end(); it ++)
+	{
+		std::cout << "Stopping " << it->first << std::endl;
+		servo_collection->write(it->first,"Velocity", 0);
+	}
+	// Or require that everyone goes into QUICK_STOP state
+	//servo_collection->require_servo_state(asp::ServoStates::QuickStopActive);
+}
+
+void *ctrl_fct(void *args){
+	
+	std::cout << "Ctrl function up" << std::endl;
+	
     // Prints out a summary of the servos
-    std::cout << servo_collection << std::endl;
- 
+    asp::ServoCollection *servo_collection = (asp::ServoCollection *)args;
+    std::cout << *servo_collection << std::endl;
+ 	bool verbose = false;
+ 	
     // Setting servo(s) in operational
-    servo_collection.connect();
-    servo_collection.require_ecat_state(asp::EthercatStates::Operational);
-    servo_collection.require_servo_state(asp::ServoStates::OperationEnabled);
+    servo_collection->connect();
+    servo_collection->require_ecat_state(asp::EthercatStates::Operational);
+    servo_collection->require_servo_state(asp::ServoStates::OperationEnabled);
 
+	// Signal the watchdog that I'm ready to send commands
+	sem_post(&semaphore);
+	
     // test loop
     int actual_velocity = 0;
     while (true) {
@@ -35,7 +76,7 @@ int main(int argc, char** argv) {
         std::string servo_name = input_str.substr(0, input_str.find(delimiter));
 
 	std::cout << "Position of " << servo_name << ": " 
-		<< servo_collection.read_INT32(servo_name, "Position") 
+		<< servo_collection->read_INT32(servo_name, "Position") 
 		<< std::endl;
 
         int target_velocity = std::stoi(input_str.substr(input_str.find(delimiter)+1,input_str.size() - input_str.find(delimiter) - 1));
@@ -46,21 +87,78 @@ int main(int argc, char** argv) {
             target_velocity = -1310720;
         }
 
-        servo_collection.set_verbose(verbose);
+        servo_collection->set_verbose(verbose);
         // Writing velocity to servo
-        servo_collection.write(servo_name,"Velocity",target_velocity);
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        servo_collection.set_verbose(false);
+        servo_collection->write(servo_name,"Velocity",target_velocity);
+        
+        // Say to the watchdog that we've sent the command
+        write_cnt =(write_cnt +1) % WRITE_MAX;
+        
+        // Sleep
+        std::this_thread::sleep_for(std::chrono::milliseconds(CTRL_LOOP));
+        servo_collection->set_verbose(false);
 
 	std::cout << "Position of " << servo_name << ": " 
-		<< servo_collection.read_INT32(servo_name, "Position") 
+		<< servo_collection->read_INT32(servo_name, "Position") 
 		<< std::endl;
     }
 
-    servo_collection.set_verbose(verbose);
-    servo_collection.require_servo_state(asp::ServoStates::SwitchOnDisabled);
-    servo_collection.set_verbose(false);  
-    servo_collection.disconnect();
+    servo_collection->set_verbose(verbose);
+    servo_collection->require_servo_state(asp::ServoStates::SwitchOnDisabled);
+    servo_collection->set_verbose(false);  
+    servo_collection->disconnect();
+    std::cout << "Ctrl exiting" << std::endl;
+    pthread_exit(0);
+}
 
-    return 0;
+void cleanup(){	
+    sem_destroy(&semaphore);
+}
+
+int main(int argc, char** argv) {
+	
+	pthread_t wdog_tid, ctrl_tid, main_tid;
+	
+    bool verbose = true;
+    if (argc > 1) {
+		verbose = false;
+    }
+    
+	main_tid = pthread_self();
+	
+	// Loading servo settings from XML	- temporarly global
+	asp::ServoCollection servo_collection(CONFIG + VEL_CONF);
+	
+	// Create semaphore
+	sem_init(&semaphore, 0, 0);
+	
+    // Create watchdog thread
+    if(pthread_create(&wdog_tid, NULL, watchdog_fct, (void *) &servo_collection) != 0){
+    	std::cerr << "Error while creating watchdog, exiting" << std::endl;
+    	cleanup();
+    	exit(1);
+    }
+    
+    // We have the watchdog -> create the control thread
+    if(pthread_create(&ctrl_tid, NULL, ctrl_fct, (void *) &servo_collection) != 0){
+    	std::cerr << "Error while creating control thread, exiting" << std::endl;
+    	cleanup();
+    	exit(1);
+    }
+    
+    // Wait until the two threads have died
+    if(pthread_join(wdog_tid, NULL) != 0){
+    	std::cerr << "Error while joining watchdog thread, should never happen " << std::endl;
+    	/* Error while waiting for watchdog thread, 
+    	 *should never happen, perform safe exit just in case ?
+    	 */
+    	stop_all(&servo_collection);
+    }
+    
+	/**
+    * The watchdog has died (having performed the safe stop procedure), this 
+    * *should* happen only if it detected that the control loop died/got stuck.
+    * We can then kill the main thread (hence the control thread).
+    */
+    exit(0);
 }
